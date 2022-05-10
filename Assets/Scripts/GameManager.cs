98d27ef130a86a39942954fd7dd0f8e4b2ab4168
Kai-6 <89489271+Kai-6@users.noreply.github.com>
2022-05-10 15:48:22 -0700

    I MADE IT A FRUIT NINJA CLONE
    Added a slicer object and an accompanying script to make a click activated continuous kill object with a particle trail.

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public List<GameObject> targets;
+    private float spawnRate = 2.0f;
+    void Start()
+    {
+        StartCoroutine(SpawnTarget());    
+    }
+
+    IEnumerator SpawnTarget()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(spawnRate);
+            int index = Random.Range(0, targets.Count);
+            Instantiate(targets[index]);
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Slice.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Slice : MonoBehaviour
+{
+    public Collider sliceCollider;
+    private TrailRenderer sliceEffect;
+    private Vector3 mousePos;
+    void Start()
+    {
+        sliceEffect = gameObject.GetComponent<TrailRenderer>();
+        sliceEffect.emitting = false;
+        sliceCollider.enabled = false;
+    }
+
+    void Update()
+    {
+        mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mousePos = new Vector3(mousePos.x, mousePos.y, 0);
+        transform.position = mousePos;
+    }
+    private void OnMouseDown()
+    {
+        sliceEffect.emitting = true;
+        sliceCollider.enabled = true;
+    }
+
+    private void OnMouseUp()
+    {
+        sliceEffect.emitting = false;
+        sliceCollider.enabled = false;
+    }
+}
