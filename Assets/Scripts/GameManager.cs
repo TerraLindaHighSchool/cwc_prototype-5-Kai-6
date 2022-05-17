706a364b605723949a983845d663541ff40a8034
Kai-6 <89489271+Kai-6@users.noreply.github.com>
2022-05-17 15:45:35 -0700

    loads of things

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,13 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
     public List<GameObject> targets;
-    private float spawnRate = 2.0f;
+    private float spawnRate = 1.0f;
+    private int score;
+    public TextMeshProUGUI scoreText;
+
     void Start()
     {
+        UpdateScore(0);
         StartCoroutine(SpawnTarget());    
     }
 
@@ -19,6 +24,12 @@ public class GameManager : MonoBehaviour
             int index = Random.Range(0, targets.Count);
             Instantiate(targets[index]);
         }
+    }
+
+    public void UpdateScore(int scoreToAdd)
+    {
+        score += scoreToAdd;
+        scoreText.text = "Score: " + score;
     }
     // Update is called once per frame
     void Update()
--- Assets/Scripts/Target.cs
+++ Assets/Scripts/Target.cs
@@ -5,17 +5,23 @@ using UnityEngine;
 public class Target : MonoBehaviour
 {
     private Rigidbody targetRb;
-    private float minSpeed = 13;
+    private float minSpeed = 14;
     private float maxSpeed = 17;
     private float maxTorque = 10;
     private float xRange = 4;
     private float ySpawnPos = -6;
+    public ParticleSystem explosion;
+    private GameManager gameManager;
+    public int pointValue;
+    private int targetLayer = 8;
     void Start()
     {
+        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
         targetRb = GetComponent<Rigidbody>();
         targetRb.AddForce(RandomForce(), ForceMode.Impulse);
         targetRb.AddTorque(RandomTorque(), RandomTorque(), RandomTorque(), ForceMode.Impulse);
         transform.position = RandomSpawnPos();
+        Physics.IgnoreLayerCollision(8, 8);
     }
 
     // Update is called once per frame
@@ -24,14 +30,25 @@ public class Target : MonoBehaviour
         
     }
 
-    private void OnMouseDown()
-    {
-        Destroy(gameObject);
-    }
-
     private void OnTriggerEnter(Collider other)
     {
-        Destroy(gameObject);
+        if (other.tag == "Slicer")
+        {
+            return;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.tag == "Slicer")
+        {
+            gameManager.UpdateScore(pointValue);
+            Destroy(gameObject);
+            Instantiate(explosion, transform.position, explosion.transform.rotation);
+        }
     }
     Vector3 RandomForce()
     {
